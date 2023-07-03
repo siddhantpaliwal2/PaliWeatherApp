1774cd33117da57b18f2ac311e79512f15cc18d1
siddhantpaliwal2 <paliwal.siddhant@gmail.com>
2023-07-03 15:03:31 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Siddhant Paliwal on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
